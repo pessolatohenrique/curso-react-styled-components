a65fe6f4ae20dfe14038434256d2d35e3a9f25d0
pessolatohenrique <pessolatohenrique@gmail.com>
2021-09-01 22:31:28 -0300

    Estilo global

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
+import GlobalStyle from "./Components/GlobalStyle";
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Cabecalho />
       <Container />
     </>
