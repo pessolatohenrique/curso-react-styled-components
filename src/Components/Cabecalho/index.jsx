9f1952fc3ab8b4ace7be3df81066d40252c31eac
pessolatohenrique <pessolatohenrique@gmail.com>
2021-09-04 14:56:28 -0300

    Modificacao de estilo de acordo com props

--- src/Components/Cabecalho/index.jsx
+++ src/Components/Cabecalho/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import bank_logo from "../../assets/images/bank_logo.svg";
 import styled from "styled-components";
-import { MAIN_COLOR } from "../DefaultColors";
+import { MAIN_COLOR, TEXT_COLOR } from "../DefaultColors";
 
 const CabecalhoSection = styled.div`
   background-color: ${MAIN_COLOR};
@@ -12,17 +12,26 @@ const CabecalhoSection = styled.div`
   align-items: center;
 `;
 
+const Button = styled.a`
+  text-align: center;
+  border-radius: 3px;
+  padding: 5px 20px;
+  margin: 0 10px;
+  font-weight: 600;
+  border: 2px solid white;
+  color: ${(props) => (props.primary ? MAIN_COLOR : TEXT_COLOR)};
+  background: ${(props) => (props.primary ? TEXT_COLOR : "transparent")};
+`;
+
 const Cabecalho = () => {
   return (
     <CabecalhoSection>
       <img className="imagem-logo" src={bank_logo} alt="Logo Smart Bank" />
       <div>
-        <a className="btn-secundario" href="https://google.com">
+        <Button primary href="https://google.com">
           Ajuda
-        </a>
-        <a className="btn-primario" href="https://google.com">
-          Sair
-        </a>
+        </Button>
+        <Button href="https://google.com">Sair</Button>
       </div>
     </CabecalhoSection>
   );
--- src/Components/DefaultColors.js
+++ src/Components/DefaultColors.js
@@ -1,1 +1,2 @@
 export const MAIN_COLOR = "#41d3be";
+export const TEXT_COLOR = "#fff";
